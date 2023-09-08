2cb61750e1fccef022c49b327c39fdb1385b3c15
Fyodorov Pavel <PFedorov@BroadViewSoftware.com>
2023-09-08 15:27:20 +0300

    Update /error mapping

--- src/main/java/com/example/webfluxdemo/service/MainService.java
+++ src/main/java/com/example/webfluxdemo/service/MainService.java
@@ -4,17 +4,27 @@ import com.example.webfluxdemo.Model.Item;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import java.util.List;
+
 @Service
 public class MainService {
 
+  List<String> strings = List.of("First", "Second", "Third", "Fourth");
+
   public Flux<Item> getNormal() {
-    return Flux.just("First", "Second", "Third", "Fourth")
+    return Flux.fromIterable(strings)
       .map(Item::new);
   }
 
   public Flux<Item> getError(){
     return Flux.create(sink -> {
+
+      strings.forEach(s -> sink.next(new Item(s)));
+
       sink.error(new Exception("ERROR!!!"));
+
+      sink.next(new Item("Fifth"));
+
     });
   }
 
